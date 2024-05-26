cba6e323925251836b6183f024af5aaf7cf3183d
Iqbal Bhatti <ib874939@gmail.com>
2024-05-26 18:35:35 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# istanbuldondurma.github.io
+Istanbul Dondurma Ice Cream Official Website
+
+Now Turkish Ice Cream available in different cities of Pakistan. This website contain the information of Turkish Tricky Ice Cream (Istanbul Dondurma)
+
+Powered by Shots International Technology Pvt Ltd.
---
+++ js/main.js
@@ -0,0 +1,171 @@
+(function ($) {
+    "use strict";
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Service and team carousel
+    $(".service-carousel, .team-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Product carousel
+    $(".product-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
+
+
+// this code for email subscribing
+var validateBtn = document.querySelector("#validateBtn");
+var emailInput = document.querySelector("#email");
+var errorDiv = document.querySelector("#error");
+
+function validateEmailFormat(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    // document.querySelector('.subscription').classList.add('done');
+    // validateBtn.addEventListener("click", function() {
+        var email = emailInput.value.trim();
+        if (email === "") {
+            errorDiv.innerHTML = "Please! Enter your Email address";
+          }
+          else if (validateEmailFormat(email)) {
+              errorDiv.style.display = "none";
+              document.querySelector('.subscription').classList.add('done');
+              
+            validateBtn.addEventListener("click", function() {
+                setTimeout(function() {
+                    emailInput.disabled = true;
+                    this.disabled = true;
+                }, 1200)
+            })
+          } else {
+            errorDiv.style.display = "block";
+            errorDiv.style.color = "red";
+            errorDiv.textContent = email+" is not valid Email";
+          }
+    // })
+});
